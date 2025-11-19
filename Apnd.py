d8230db70297670ed344d92aa3b020779c4b3677
Luis Felipe Graciolli <shiwa101@proton.me>
2025-11-19 18:33:04 -0300

    Define formato do automato a ser lido

--- Apnd.py
+++ Apnd.py
@@ -1,7 +1,7 @@
 from typing import List
 
 class APND():
-    def __init__(self, alfabeto: set, estados: set, estado_inicial: str, estados_finais: set, transicoes: List[List[str]]) -> None:
+    def __init__(self, alfabeto: set, estados: set, estado_inicial: str, estados_finais: set, transicoes: List[List[str]]):
         self.alfabeto = alfabeto
         self.estados = estados
         self.estado_inicial = estado_inicial
@@ -12,3 +12,13 @@ class APND():
     def check_alfabeto(self, cadeia: str) -> bool:
         """Verifica se todos os símbolos da cadeia pertencem ao alfabeto."""
         return set(cadeia).issubset(self.alfabeto)
+    
+    def check_cadeia(self, cadeia: str) -> bool:
+        """Verifica se a cadeia está vazia."""
+        return len(cadeia) == 0
+    
+    def processa_cadeia(self, cadeia: str):
+        """Processa a cadeia usando o autômato APND."""
+        print("\n[INFO] Iniciando o processamento da cadeia...")
+        pass
+        
